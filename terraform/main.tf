10b7b2e7c961733c817e78502c09244f72ffee3c
mdsdsardar <83182506+mdsdsardar@users.noreply.github.com>
2024-06-09 14:13:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_lightsail_instance" "my_instance" {
   name              = "saad-lightsail-instance"
   availability_zone = "ap-south-1a"
   blueprint_id      = "amazon_linux_2"
-  bundle_id         = "micro_3_0"
+  bundle_id         = "micro_3_1"
 
   user_data = file("${path.module}/launch-script.sh")
 
