01f2dbbc7b713f9cdfb99cab6535c5a046e954b1
mdsdsardar <83182506+mdsdsardar@users.noreply.github.com>
2024-06-09 14:07:04 +0530

    Update main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_lightsail_instance" "my_instance" {
+  name              = "saad-lightsail-instance"
+  availability_zone = "ap-south-1a"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "micro_3_0"
+
+  user_data = file("${path.module}/launch-script.sh")
+
+  tags = {
+    Name = "MyLightsailInstance"
+  }
+}
