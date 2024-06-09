db8202a19bcab399702cb6aeb2af78a25e2384ad
mdsdsardar <83182506+mdsdsardar@users.noreply.github.com>
2024-06-09 14:52:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,12 @@ resource "aws_lightsail_instance" "my_instance" {
   availability_zone = "ap-south-1a"
   blueprint_id      = "amazon_linux_2"
   bundle_id         = "micro_3_1"
-
   user_data = file("${path.module}/launch-script.sh")
-
   tags = {
     Name = "MyLightsailInstance"
   }
+}
+
+output "instance_public_ip" {	
+  value = data.aws_lightsail_instance.my_instance.public_ip_address	
 }
