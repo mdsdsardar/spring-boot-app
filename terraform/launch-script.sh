68c002299b63328c651745edafa5e2c68b4d71b0
mdsdsardar <83182506+mdsdsardar@users.noreply.github.com>
2024-06-09 14:23:01 +0530

    Update launch-script.sh

--- terraform/launch-script.sh
+++ terraform/launch-script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo yum install -y docker
+sudo yum install -y git
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+git clone https://github.com/mdsdsardar/spring-boot-app.git
+cd spring-boot-app
 docker-compose up -d
 sleep 60
